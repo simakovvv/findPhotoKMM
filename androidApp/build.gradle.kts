3153b9d5340323d4492ef680e7d18a47a310663b
simakovvv-areality <simakov.v@areality.app>
2021-11-18 19:07:39 +0300

    Adds getPack compose and basic navigation

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -18,10 +18,40 @@ android {
         }
     }
 }
+val composeVersion = findProperty("version.compose") as String
 
 dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    // Tooling support (Previews, etc.)
+    implementation("androidx.compose.ui:ui:1.0.5")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
+    // Compose Material Design
+    implementation("androidx.compose.material:material:$composeVersion")
+    // When using a MDC theme
+    implementation ("com.google.android.material:compose-theme-adapter:1.1.0")
+
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:1.4.0")
+
+    //Compose utils
+    implementation("com.google.accompanist:accompanist-insets:0.20.0")
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.20.0")
+
+    // Integration with activities
+    implementation ("androidx.activity:activity-compose:1.4.0")
+
+    // Integration with ViewModels
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
+
+    // Nvigation between fragments
+    implementation (  "androidx.navigation:navigation-compose:2.4.0-beta02")
+
+    // UI Tests
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.0.5")
 }
--- androidApp/src/main/java/com/electrolux/findphoto/android/MainActivity.kt
+++ androidApp/src/main/java/com/electrolux/findphoto/android/MainActivity.kt
@@ -4,17 +4,81 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.electrolux.findphoto.Greeting
 import android.widget.TextView
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.Navigator
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.electrolux.findphoto.android.compose.ui.AppTheme
+import com.google.accompanist.insets.LocalWindowInsets
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.rememberInsetsPaddingValues
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /*setContentView(R.layout.activity_main)
 
         val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        tv.text = greet()*/
+
+        setContent {
+            AppTheme {
+                ProvideWindowInsets {
+                    Box(
+                        Modifier.padding(
+                            rememberInsetsPaddingValues(
+                                insets = LocalWindowInsets.current.systemBars,
+                                applyStart = true,
+                                applyTop = false,
+                                applyEnd = true,
+                                applyBottom = false
+                            )
+                        )
+                    ) {
+                        SetupNavigation()
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun SetupNavigation() {
+        val navController = rememberNavController()
+        NavHost(navController, startDestination = "pictureList") {
+            composable(route = "pictureList") {
+                PictureList(navController)
+            }
+            composable(route = "pictureDetails") {
+                PictureDetailsScreen(navController)
+            }
+        }
+    }
+
+    @Composable
+    fun PictureList( navController: NavController) {
+        Button(onClick = { navController.navigate("pictureDetails") }) {
+            Text("Recycler view will be here")
+        }
+    }
+
+    @Composable
+    fun PictureDetailsScreen(navController: NavController) {
+        Button(onClick = { navController.navigate("pictureList") }) {
+            Text("Go back to list!")
+        }
     }
 }
